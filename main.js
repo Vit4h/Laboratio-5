d38cf0d8d712c6dc88305c2e758a6d35246b79ab
Vit4h <wjherrera@correo.url.edu.gt>
2023-04-24 16:54:02 -0600

    no funciona chequear

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const lineReader = require('line-reader');
 lineReader.eachLine('./input_challenge_lab_2.jsonl.txt', function(line, last) {
   
   let jasonObject = JSON.parse(line);
-  //console.log(jasonObject);
-  //console.log(jasonObject.mensajes);
   
-  console.log(jasonObject["mensajes"]);
+  console.log(jasonObject.input2);
+  
+ 
   if(last) {
     console.log('Last line printed.');
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
