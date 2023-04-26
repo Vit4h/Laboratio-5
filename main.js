d5093e3b2fdf096de9cbd4c683c74d4d10c40cc8
Vit4h <wjherrera@correo.url.edu.gt>
2023-04-26 17:20:14 -0600

    forma alternativa de leer JSON

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-const lineReader = require('line-reader');
-
-lineReader.eachLine('./input_challenge_lab_2.jsonl.txt', function(line, last) {
-  
-  let jasonObject = JSON.parse(line);
-  
-  console.log(jasonObject.input2);
-  
- 
-  if(last) {
-    console.log('Last line printed.');
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
-  }
+function lecturaJason(){
+  var fs = require('fs');
+// este metodo denominado fs.readfile sirve para leer el archivo
+fs.readFile('./input_challenge_lab_2.jsonl', 'utf8', function(err, data){
+		// Desplegando la lectura del archivo
+	console.log(data);
 });
+console.log('readFile called');
+
+}
+lecturaJason();
