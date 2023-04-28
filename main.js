1db0da4b4cf86eecde8c365ef00310ab17e78d56
Vit4h <wjherrera@correo.url.edu.gt>
2023-04-27 23:29:47 -0600

    poniendo toda la lectura en una funcion

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const lineReader = require('line-reader');
-
+function lecturaJason(){
+  const lineReader = require('line-reader');
 lineReader.eachLine('./input_challenge_lab_2.jsonl', function(line, last) {
-  
-  let jasonObject = JSON.parse(line);
-  //console.log(jasonObject);
-  console.log(jasonObject.input1);
-  
-  
-  if(last) {
+    const jason = JSON.parse(line);
+  console.log(jason.input1);
+    if(last) {
     console.log('Last line printed.');
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
   }
 });
+}
+lecturaJason();
