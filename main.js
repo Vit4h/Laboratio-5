732760a000cabb0fb2aa04e34aa1a27bee935d46
Vit4h <wjherrera@correo.url.edu.gt>
2023-04-29 16:11:33 -0600

    revision

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function lecturaJason(){
+  
   const lineReader = require('line-reader');
 lineReader.eachLine('./input_challenge_lab_2.jsonl', function(line, last) {
     const jason = JSON.parse(line);
-  console.log(jason.input1);
+    console.log(jason);
     if(last) {
     console.log('Last line printed.');
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
@@ -10,4 +11,19 @@ lineReader.eachLine('./input_challenge_lab_2.jsonl', function(line, last) {
   }
 });
 }
-lecturaJason();
+console.log(lecturaJason());
+
+// Node.js program to demonstrate
+// the fs.readFile() method
+
+// Include fs module
+/*var fs = require('fs');
+
+// Use fs.readFile() method to read the file
+fs.readFile('./input_challenge_lab_2.jsonl', 'utf8', function(err, data){
+	const lectura = JSON.parse(data);
+	// Display the file content
+	console.log(lectura);
+});*/
+
+
