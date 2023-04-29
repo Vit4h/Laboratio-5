276c38d5ebdca4cbc6275d57587bb310b4180e72
Vit4h <wjherrera@correo.url.edu.gt>
2023-04-28 22:49:33 -0600

    prueba de import y require

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
-function lecturaJason(){
-  const lineReader = require('line-reader');
-lineReader.eachLine('./input_challenge_lab_2.jsonl', function(line, last) {
-    const jason = JSON.parse(line);
-  console.log(jason.input1);
-    if(last) {
-    console.log('Last line printed.');
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
-  }
-});
-}
-lecturaJason();
+const prueba = require('./prueba');
+console.log(prueba);
---
+++ prueba.js
@@ -0,0 +1,14 @@
+function lecturaJason(){
+    const lineReader = require('line-reader');
+  lineReader.eachLine('./input_challenge_lab_2.jsonl', function(line, last) {
+      const jason = JSON.parse(line);
+    console.log(jason.input1);
+      if(last) {
+      console.log('Last line printed.');
+      const used = process.memoryUsage().heapUsed / 1024 / 1024;
+      console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+    }
+  });
+  }
+  module.exports= {lecturaJason};
+
